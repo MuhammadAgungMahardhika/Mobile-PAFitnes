2d4e270ce2bc86608281e4d980e663a8b67f1de0
Amanda Noverdina Putri <amandanove43@gmail.com>
2021-11-27 16:52:47 +0700

    git remote add Origin https://github.com/MuhammadAgungMahardhika/Pafitness.git

--- app/src/main/java/com/example/pafitness/ResetpasswordActivity.java
+++ app/src/main/java/com/example/pafitness/ResetpasswordActivity.java
@@ -2,7 +2,10 @@ package com.example.pafitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResetpasswordActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class ResetpasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resetpassword);
+
+        Button ResetPassword= (Button) findViewById(R.id.buttonResetpassword;
+
+        ResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(ResetpasswordActivity.this,NewpasswordActivity.class);
+                ResetpasswordActivity.this.startActivity(intent2);
+
+            }
+        });
     }
 }
