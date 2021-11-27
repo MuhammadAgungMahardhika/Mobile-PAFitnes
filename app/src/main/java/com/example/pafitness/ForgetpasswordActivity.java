66514e17b29b22a5aac60f4a784094f6f2d17897
Amanda Noverdina Putri <amandanove43@gmail.com>
2021-11-27 16:32:10 +0700

    git remote add Origin https://github.com/MuhammadAgungMahardhika/Pafitness.git

--- app/src/main/java/com/example/pafitness/ForgetpasswordActivity.java
+++ app/src/main/java/com/example/pafitness/ForgetpasswordActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ForgetpasswordActivity extends AppCompatActivity {
@@ -14,15 +15,16 @@ public class ForgetpasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgetpassword);
 
-        TextView forgetPassword= (TextView) findViewById(R.id.textView9);
+        Button forgetPassword = (Button) findViewById(R.id.buttonForgetpassword);
 
         forgetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(ForgetpasswordActivity.this, LoginActivity.class);
+                Intent intent2 = new Intent(ForgetpasswordActivity.this,ResetpasswordActivity.class);
                 ForgetpasswordActivity.this.startActivity(intent2);
 
             }
         });
+
     }
 }
