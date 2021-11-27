4f3dd6991e1b272505c02166d21e509b6d55a96b
Your Name <you@example.com>
2021-11-27 15:15:24 +0700

    git remote add Origin https://github.com/MuhammadAgungMahardhika/Pafitness.git

--- app/src/main/java/com/example/pafitness/GenderActivity.java
+++ app/src/main/java/com/example/pafitness/GenderActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-s
+
 public class GenderActivity extends AppCompatActivity {
 
     @Override
