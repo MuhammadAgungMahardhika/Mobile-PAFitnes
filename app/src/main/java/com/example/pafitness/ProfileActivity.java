53299ea0a6044adb0999b165e8bcc428c57f99b2
Your Name <you@example.com>
2021-11-28 13:25:52 +0700

    git remote add Origin https://github.com/MuhammadAgungMahardhika/Pafitness.git

--- app/src/main/java/com/example/pafitness/ProfileActivity.java
+++ app/src/main/java/com/example/pafitness/ProfileActivity.java
@@ -1,50 +1,28 @@
 package com.example.pafitness;
 
-import android.content.Intent;
+        import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        //Home intent
-        ImageButton home = (ImageButton) findViewById(R.id.homeButton);
+        //EditProfile intent
 
-        home.setOnClickListener(new View.OnClickListener() {
+        Button editProfile = (Button) findViewById(R.id.button4);
+
+        editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(ProfileActivity.this,HomeActivity.class);
-                ProfileActivity.this.startActivity(intent2);
-
-            }
-        });
-
-        //Arroundyou intent
-        ImageButton arroundYou = (ImageButton) findViewById(R.id.arroundYouButton);
-
-        arroundYou.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent2 = new Intent(ProfileActivity.this,HomeActivity.class);
-                ProfileActivity.this.startActivity(intent2);
-
-            }
-        });
-
-        //Profile intent
-        ImageButton profile = (ImageButton) findViewById(R.id.profileButton);
-
-        profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent2 = new Intent(ProfileActivity.this,ProfileActivity.class);
+                Intent intent2 = new Intent(ProfileActivity.this,EditprofileActivity.class);
                 ProfileActivity.this.startActivity(intent2);
 
             }
