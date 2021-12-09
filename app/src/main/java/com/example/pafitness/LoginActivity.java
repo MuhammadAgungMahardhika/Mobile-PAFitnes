c1ab005cebafb3a9c2d7f5a51d27130b27e4cc9b
Your Name <you@example.com>
2021-12-09 09:57:04 +0700

    git remote add Origin https://github.com/MuhammadAgungMahardhika/Pafitness.git

--- app/src/main/java/com/example/pafitness/LoginActivity.java
+++ app/src/main/java/com/example/pafitness/LoginActivity.java
@@ -8,14 +8,50 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button Home;
+    EditText username,password;
+    String email,pass;
+    String cekEmail,cekPass1;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Login intent
+        username = findViewById(R.id.editUsername);
+        password =  findViewById(R.id.editPassword);
+        Home = findViewById(R.id.Login);
+        mAuth = FirebaseAuth.getInstance();
+        Home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                cekEmail = username.getText().toString();
+                cekPass1 = password.getText().toString();
+
+                if(cekEmail.isEmpty() || cekPass1.isEmpty() ){
+                    Toast.makeText(LoginActivity.this, "Login failed, please fill email and password",
+                            Toast.LENGTH_SHORT).show();
+                }else if(cekPass1.length() <=6){
+                    Toast.makeText(LoginActivity.this, "Login failed , Password's characters less than 6 characters",
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    cekLogin();
+                }
+            }
+        });
+
 
         //sing up  intent
         TextView Signup2 = (TextView) findViewById(R.id.textView8);
@@ -40,30 +76,32 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        //Login intent
-        Button Home = (Button) findViewById(R.id.Login);
-        EditText username = (EditText) findViewById(R.id.editUsername);
-        EditText password = (EditText) findViewById(R.id.editPassword);
 
-        
-        Home.setOnClickListener(new View.OnClickListener() {
+    }
+
+    //Cek kondisi login dengan Firebase
+    private void cekLogin() {
+        email = username.getText().toString();
+        pass = password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
-                if (username.getText().toString().equals("admin")
-                        && password.getText().toString().equals("admin")){
-
-                    Toast.makeText(getApplicationContext(),"LOGIN BERHASIL", Toast.LENGTH_LONG).show();
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Login success",
+                            Toast.LENGTH_SHORT).show();
                     Intent intent2 = new Intent(LoginActivity.this, HomeActivity.class);
-                    LoginActivity.this.startActivity(intent2); 
-                }else{
-                    Toast.makeText(getApplicationContext(),"USERNAME ATAU PASSWORD SALAH",
-                            Toast.LENGTH_LONG).show();
+                    LoginActivity.this.startActivity(intent2);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(LoginActivity.this, "Login failed, please check your email and password again!",
+                            Toast.LENGTH_SHORT).show();
+
                 }
 
-
+                // ...
             }
-
         });
-
     }
 }
--- app/src/main/java/com/example/pafitness/SignupActivity.java
+++ app/src/main/java/com/example/pafitness/SignupActivity.java
@@ -4,28 +4,88 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignupActivity extends AppCompatActivity {
+
+    Button SignUp;
+    EditText username,password, password2;
+    String email, pass,pass2;
+    String cekEmail,cekPass1,cekPass2;
+    Boolean samePass;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        Button SignUp = (Button) findViewById(R.id.buttonUp2);
+        username = findViewById(R.id.editemail);
+        password =  findViewById(R.id.editPassword2);
+        password2 = findViewById(R.id.editPassword3);
+        SignUp = findViewById(R.id.buttonUp2);
+        mAuth = FirebaseAuth.getInstance();
+
 
         SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(SignupActivity.this,GenderActivity.class);
-                SignupActivity.this.startActivity(intent2);
+                cekEmail = username.getText().toString();
+                cekPass1 = password.getText().toString();
+                cekPass2 = password2.getText().toString();
+                samePass = cekPass1.equals(cekPass2);
+                if(cekEmail.isEmpty() || cekPass1.isEmpty() || cekPass2.isEmpty()){
+                    Toast.makeText(SignupActivity.this, "Registry failed, please fill email and password",
+                            Toast.LENGTH_SHORT).show();
+                }else if(cekPass1.length() <=6){
+                    Toast.makeText(SignupActivity.this, "Registry failed , Password's characters less than 6 characters",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else if (samePass){
+                    Register();
+
+                }else{
+                    Toast.makeText(SignupActivity.this, "Registry failed, please check your password again",
+                            Toast.LENGTH_SHORT).show();
+                }
+
 
             }
         });
 
-}
+    }
 
+    private void Register() {
+        email = username.getText().toString();
+        pass = password.getText().toString();
+        pass2 = password2.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, pass2)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignupActivity.this, "Registry Success",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent2 = new Intent(SignupActivity.this, GenderActivity.class);
+                            SignupActivity.this.startActivity(intent2);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignupActivity.this, "Registry Failed",
+                                    Toast.LENGTH_SHORT).show();
 
+                        }
+
+                        // ...
+                    }
+                });
+    }
 }
