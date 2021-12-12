0c0f4734f63cf437c8c8e1af84e2d3515fc58bf3
Your Name <you@example.com>
2021-12-12 13:52:35 +0700

    git remote add Origin https://github.com/MuhammadAgungMahardhika/Pafitness.git

--- app/src/main/java/com/example/pafitness/MapsActivity.java
+++ app/src/main/java/com/example/pafitness/MapsActivity.java
@@ -85,8 +85,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(-0.92795,100.413122);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Your Marker"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
